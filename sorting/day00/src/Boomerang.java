f9de9c238e667c9a92f8e88763180e5538e53256
Camille Xue <xue.camille@gmail.com>
2019-02-14 21:02:59 -0500

    finished with sorting day 00 hw

--- sorting/day00/src/Boomerang.java
+++ sorting/day00/src/Boomerang.java
@@ -1,8 +1,37 @@
+import java.util.*;
+
 public class Boomerang {
 
     public static int numberOfBoomerangs(int[][] points) {
-        // TODO
-        return 0;
-    }
-}
+        int res = 0;
 
+        Map<Integer, Integer> map = new HashMap<>();
+        // for each possible boomerang vertex
+        for(int i = 0; i < points.length; i++){
+
+            // calculate distances with each other point
+            for(int j = 0; j < points.length; j++) {
+                if (i == j) {
+                    continue;
+                } else {
+                    // calculate dist and increment val for that dist key
+                    int dist = getDistance(points[i], points[j]);
+                    map.put(dist, map.getOrDefault(dist, 0) + 1);
+                }
+            }
+            for(int val : map.values()) {
+                res += val * (val-1);   // sum up boomerangs for each vertex
+                }
+            map.clear();
+        }
+        return res;
+    }
+
+    public static int getDistance(int[] a, int[] b) {
+        int dx = a[0] - b[0];
+        int dy = a[1] - b[1];
+
+        return dx*dx + dy*dy;
+    }
+
+}
--- sorting/day00/src/MergeSort.java
+++ sorting/day00/src/MergeSort.java
@@ -1,7 +1,9 @@
+import java.util.*;
 
 public class MergeSort extends SortAlgorithm {
 
     private static final int INSERTION_THRESHOLD = 10;
+    private InsertionSort insertionSort = new InsertionSort();
 
     /**
      * This is the recursive step in which you split the array up into
@@ -9,16 +11,30 @@ public class MergeSort extends SortAlgorithm {
      * Use Insertion Sort if the length of the array is <= INSERTION_THRESHOLD
      *
      * TODO
-     * Best-case runtime:
-     * Worst-case runtime:
-     * Average-case runtime:
+     * Best-case runtime: O(1)
+     * Worst-case runtime: O(NlogN)
+     * Average-case runtime: O(NlogN)
      *
-     * Space-complexity:
+     * Space-complexity: O(N)
      */
     @Override
     public int[] sort(int[] array) {
-        // TODO
-        return new int[0];
+        if(array.length < INSERTION_THRESHOLD) {
+            return insertionSort.sort(array);
+        }
+        int half = array.length/2;
+        int[] lefthalf = new int[half];
+        int[] righthalf = new int[array.length-half];
+
+        System.arraycopy(array, 0, lefthalf, 0, half);
+        System.arraycopy(array, half, righthalf, 0, array.length - half);
+
+        int[] sortedLeft = sort(lefthalf);
+        int[] sortedRight = sort(righthalf);
+
+        array = merge(sortedLeft, sortedRight);
+
+        return array;
     }
 
     /**
@@ -26,8 +42,29 @@ public class MergeSort extends SortAlgorithm {
      * all elements in a and b. A test for this method is provided in `SortTest.java`
      */
     public int[] merge(int[] a, int[] b) {
-        // TODO
-        return new int[0];
+        int[] array = new int[a.length + b.length];
+        int i = 0;
+        int j = 0;
+
+        for(int k = 0; k < array.length; k++) {
+            if(i == a.length){
+                array[k] = b[j];
+                j++;
+            }
+            else if (j == b.length) {
+                array[k] = a[i];
+                i++;
+            }
+            else if(a[i] < b[j]) {
+                array[k] = a[i];
+                i++;
+            }
+            else {
+                array[k] = b[j];
+                j++;
+            }
+        }
+        return array;
     }
 
 }
